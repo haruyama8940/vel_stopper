24f96e03063e5ec932bee32421d2224192dd2c27
haruyama8940 <yaruo0406@gmail.com>
2022-11-08 03:32:41 +0900

    fix sleep

--- src/vel_stopper.cpp
+++ src/vel_stopper.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <std_srvs/SetBool.h>
+#include <std_srvs/Trigger.h>
 #include <geometry_msgs/Twist.h>
 
 class vel_stopper
@@ -11,14 +12,19 @@ public:
                               std_srvs::SetBool::Response &traf_resp);
     bool stop_vel_callback(std_srvs::SetBool::Request &stop_req,
                            std_srvs::SetBool::Response &stop_resp);
+    bool next_callback(std_srvs::Trigger::Request &next_req,
+                       std_srvs::Trigger::Response &next_resp);
     void cmd_callback(const geometry_msgs::Twist::ConstPtr& cmd_vel_msg);
     void send_vel();
+
 private:
     geometry_msgs::Twist pub_vel,vel;
     ros::Publisher vel_pub;
-    ros::ServiceServer traffic_call,stop_call;
+    ros::ServiceServer traffic_call,stop_call,next_call;
     bool traffic_vel_flg = false;
     bool stop_vel_flg = false;
+    bool stop_count_flg = true;
+    bool stop_send_flg =false;
 
 };
 vel_stopper::vel_stopper() :
@@ -28,6 +34,8 @@ nh_()
                                         &vel_stopper::traffic_vel_callback,this);
     stop_call = nh_.advertiseService("stop_service",
                                         &vel_stopper::stop_vel_callback,this);
+    next_call = nh_.advertiseService("next_waypoint",
+                                        &vel_stopper::next_callback,this);
     vel_pub =nh_.advertise<geometry_msgs::Twist>("stop_vel",1);
 }
 
@@ -42,12 +50,19 @@ bool vel_stopper::traffic_vel_callback(std_srvs::SetBool::Request &traf_req,
 bool vel_stopper::stop_vel_callback(std_srvs::SetBool::Request &stop_req,
                                     std_srvs::SetBool::Response &stop_resp)
 {
+
     stop_vel_flg = stop_req.data;
     stop_resp.message = "true";
     stop_resp.success = true;
     ROS_INFO("stop mode!");
 }
-
+bool vel_stopper::next_callback(std_srvs::Trigger::Request &next_req,
+                       std_srvs::Trigger::Response &next_resp)
+    {
+        stop_count_flg =true;
+        stop_vel_flg =false;
+    }
+    
 void vel_stopper::cmd_callback(const geometry_msgs::Twist::ConstPtr& cmd_vel_msg)
 {
     vel.linear.x = cmd_vel_msg->linear.x;
@@ -57,15 +72,31 @@ void vel_stopper::send_vel()
 {
     if(stop_vel_flg && !traffic_vel_flg)
     {
-        pub_vel.linear.x = 0.0;
-        pub_vel.angular.z = 0.0;
-        ROS_INFO("stop mode now!");
+        if (stop_count_flg)
+        {
+            sleep(4);
+            stop_count_flg = false;
+        }
+
+        else if (!stop_count_flg)
+        {
+            pub_vel.linear.x = 0.0;
+            pub_vel.angular.z = 0.0;
+            ROS_INFO("stop mode now!");
+            vel_pub.publish(pub_vel);
+        }
+        
+        
+        
     }
-    if(traffic_vel_flg && !stop_vel_flg)
+    else if(traffic_vel_flg && !stop_vel_flg)
     {
-        pub_vel.linear.x = vel.linear.x + 0.4;
+        pub_vel.linear.x = vel.linear.x + 0.2;
+        pub_vel.angular.x = vel.angular.z;
         ROS_INFO("traffic mode now!");
+        vel_pub.publish(pub_vel);
     }
+    
     else
     {
         ROS_INFO("take~~~???");
@@ -76,7 +107,7 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "vel_stopper_node");
  // ros::Rate loop_rate(1);
   vel_stopper vel_stop;
-  ros::Rate loop_rate(1);
+  ros::Rate loop_rate(5);
      while (ros::ok())
         {
             vel_stop.send_vel();
